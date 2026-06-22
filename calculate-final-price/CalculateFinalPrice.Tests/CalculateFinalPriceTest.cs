d2152b253c8da5ddb8b415728493d4cd092b6203
Lourdes Veros <lourdes.veros.alvarez@gmail.com>
2026-06-22 17:28:24 +0200

    refactor: test triangulation

--- calculate-final-price/CalculateFinalPrice.Tests/CalculateFinalPriceTest.cs
+++ calculate-final-price/CalculateFinalPrice.Tests/CalculateFinalPriceTest.cs
@@ -4,45 +4,19 @@ namespace CalculateFinalPrice.Tests;
 
 public class CalculateFinalPriceTest
 { 
-    [Test]
-    public void When_NoTax_Then_FinalPrice_Equals_Cost()
+    [TestCase(20.00, 0)]
+    [TestCase(0.00, 0)]
+    [TestCase(1.89, 0)]
+    public void When_NoTax_Then_FinalPrice_Equals_Cost(double cost, int tax)
     {
         Product product = new Product
         {
-            Cost = 20.00,
-            Tax = 0
+            Cost = cost,
+            Tax = tax
         };
 
         double finalPrice = product.CalculateFinalPrice();
 
-        Assert.That(finalPrice, Is.EqualTo(20.00).Within(0.0001));
-    }
-        
-    [Test]
-    public void When_NoCost_Then_FinalPrice_Equals_Zero()
-    {
-        Product product = new Product
-        {
-            Cost = 0.00,
-            Tax = 0
-        };
-
-        double finalPrice = product.CalculateFinalPrice();
-
-        Assert.That(finalPrice, Is.EqualTo(0.00).Within(0.0001));
-    }
-        
-    [Test]
-    public void When_CostHasDecimals_And_NoTax_Then_FinalPrice_Equals_Cost()
-    {
-        Product product = new Product
-        {
-            Cost = 1.89,
-            Tax = 0
-        };
-
-        double finalPrice = product.CalculateFinalPrice();
-
-        Assert.That(finalPrice, Is.EqualTo(1.89).Within(0.0001));
+        Assert.That(finalPrice, Is.EqualTo(cost).Within(0.0001));
     }
 }
